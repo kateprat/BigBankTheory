af9059bb0ed20c8ba6857b7c1ed48541008b21c1
aclimand <faclimandos@gmail.com>
2025-04-11 23:19:13 +0200

    docs

--- request.py
+++ request.py
@@ -28,36 +28,69 @@ if response.status_code == 200:
     score = data.get("score")
     documents = data.get("client_data", {})
 
-    if 'passport' in documents:
-        with open("passport.png", "wb") as file:
-            file.write(base64.b64decode(documents['passport']))
+    # if 'passport' in documents:
+    #     with open("passport.png", "wb") as file:
+    #         file.write(base64.b64decode(documents['passport']))
 
-    if 'profile' in documents:
-        with open("profile.docx", "wb") as file:
-            file.write(base64.b64decode(documents['profile']))
+    # if 'profile' in documents:
+    #     with open("profile.docx", "wb") as file:
+    #         file.write(base64.b64decode(documents['profile']))
 
-    if 'account' in documents:
-        with open("account_opening_form.pdf", "wb") as file:
-            file.write(base64.b64decode(documents['account']))
+    # if 'account' in documents:
+    #     with open("account_opening_form.pdf", "wb") as file:
+    #         file.write(base64.b64decode(documents['account']))
 
-    if 'description' in documents:
-        with open("description.txt", "wb") as file:
-            file.write(base64.b64decode(documents['description']))
+    # if 'description' in documents:
+    #     with open("description.txt", "wb") as file:
+    #         file.write(base64.b64decode(documents['description']))
 
-    print(f"Client ID: {client_id}")
-    print(f"Session ID: {session_id}")
-    print(f"score: {score}")
+    # print(f"Client ID: {client_id}")
+    # print(f"Session ID: {session_id}")
+    # print(f"score: {score}")
 else:
     print(f"Error: {response.status_code}")
     print(response.text)
 
-decision_payload = {
-    "decision" : "Accept", 
-    "session_id": session_id, 
-    "client_id" : client_id
-}
+while True: 
+    decision_payload = {
+        "decision" : "Accept", 
+        "session_id": session_id, 
+        "client_id" : client_id
+    }
 
-response = requests.post(decision_url, json=decision_payload, headers=headers)
+    response = requests.post(decision_url, json=decision_payload, headers=headers)
 
-print(f"status code : { response.status_code}")
-print("Response JSON:", response.json())
+    if response.status_code == 200:
+        
+        data = response.json()  
+        score_after = data.get("score")
+
+        client_id = data.get("client_id")  
+        print(f"Client ID: {client_id}")
+        print(f"score after decision: {score_after}")
+
+        if data.get("status") == "gameover" : 
+
+            documents = data.get("client_data", {})
+
+            if 'passport' in documents:
+                with open("passport.png", "wb") as file:
+                    file.write(base64.b64decode(documents['passport']))
+
+            if 'profile' in documents:
+                with open("profile.docx", "wb") as file:
+                    file.write(base64.b64decode(documents['profile']))
+
+            if 'account' in documents:
+                with open("account_opening_form.pdf", "wb") as file:
+                    file.write(base64.b64decode(documents['account']))
+
+            if 'description' in documents:
+                with open("description.txt", "wb") as file:
+                    file.write(base64.b64decode(documents['description']))
+
+            break
+    else:
+        print(f"Error: {response.status_code}")
+        print(response.text)
+        break
